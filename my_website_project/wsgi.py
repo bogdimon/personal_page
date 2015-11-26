7025e1ae480ee581a0fb2792c13ffaa6287c8de0
Bogdanis <bogdanis.d@gmail.com>
2015-11-26 14:46:01 +0100

    initial commit

---
+++ my_website_project/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for my_website_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_website_project.settings")
+
+application = get_wsgi_application()
+
+
+# heroku
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
